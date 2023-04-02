259e9023e81b022112e522822a923bce1041254f
Oscar Soanes <oscarsoanes@gmail.com>
2023-04-02 10:03:36 +0100

    Created the data on in second section. Added an ID to Wind Arrow in HTML

--- src/js/modules/layout/createSecondLayout.js
+++ src/js/modules/layout/createSecondLayout.js
@@ -1,4 +1,19 @@
+import { getTemperatureValue } from "./createTemperature";
+
 export function createSecondLayout(data) {
   console.log(data);
-  currentTemp.replaceWith(getTemperatureValue(data.temperature));
+  const feelsLike = document.getElementById("feels-like");
+  feelsLike.replaceWith(getTemperatureValue(data.feelsLike, "feels-like"));
+
+  const humidity = document.getElementById("humidity");
+  humidity.textContent = `${data.humidity}%`;
+
+  const chanceOfRain = document.getElementById("chance-of-rain");
+  chanceOfRain.textContent = `${data.chanceOfRain}%`;
+
+  const windSpeed = document.getElementById("wind-speed");
+  windSpeed.textContent = `${data.windSpeed}km/h`;
+
+  const windDirection = document.getElementById("direction");
+  windDirection.style.transform = `rotate(${data.windRotation}deg)`;
 }
